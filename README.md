160a32a308a53748e2d9e6e7c131d688b31581ed
Darshan Crout <dtcrout@gmail.com>
2019-06-01 14:40:19 -0400

    Redirect to spotitude.me

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # spotitude-webapp
 
+https://spotitude.me/
+
 Spotitude is a simple web-app which displays your recent top 25 tracks on Spotify. It's client side and the source code is available here!
--- main.js
+++ main.js
@@ -47,7 +47,7 @@ var app = new Vue({
   data: {
     client_id: 'c931a0d9f79848a3813338b5598a2369',
     scopes: 'user-top-read',
-    redirect_uri: 'https://spotitude.netlify.com',
+    redirect_uri: 'https://spotitude.me',
     // redirect_uri: 'http://localhost:8000',
     type: 'tracks',
     time_range: 'short_term',
